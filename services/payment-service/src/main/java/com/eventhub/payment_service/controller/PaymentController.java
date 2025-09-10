38535078336b443f4a9aa6c007bf7288c506a92f
ozmenab <ozmenab@gmail.com>
2025-09-10 13:45:10 +0300

    feat(payment): bootstrap payment-service (web, jpa, kafka, eureka, config)

--- services/payment-service/src/main/java/com/eventhub/payment_service/controller/PaymentController.java
+++ services/payment-service/src/main/java/com/eventhub/payment_service/controller/PaymentController.java
@@ -1,16 +1,15 @@
-package com.eventhub.order_service.controller;
+package com.eventhub.payment_service.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/orders")
-public class OrderController {
+@RequestMapping("/payments")
+public class PaymentController {
 
     @GetMapping("/ping")
-    public String sayHello()
-    {
-        return "order:hello";
+    public String sayHello(){
+        return "payment: hello";
     }
 }
